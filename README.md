19a8c6a7858b85008e1ff68f627e0a69ec4198fd
tbowenmsu <81266672+tbowenmsu@users.noreply.github.com>
2021-09-22 13:53:41 -0400

    updated code after rename, updated README with video links and profile links

---
+++ README.md
@@ -0,0 +1,51 @@
+# E-Commerce Backend
+
+## About
+Internet retail, also known as **e-commerce**, is the largest sector of the electronics industry, generating an estimated $29 trillion in 2019. E-commerce platforms like Shopify and WooCommerce provide a suite of services to businesses of all sizes. Due to their prevalence, understanding the fundamental architecture of these platforms will benefit all full-stack web developers. 
+
+This is framework code for a E-Commerce backend with demo videos showing how to start and seed a database in VScode and the operational GET, POST, PUT, and DELETE routes.  
+
+## User Story
+```md
+GIVEN a functional Express.js API
+WHEN I add my database name, MySQL username, and MySQL password to an environment variable file
+THEN I am able to connect to a database using Sequelize
+WHEN I enter schema and seed commands
+THEN a development database is created and is seeded with test data
+WHEN I enter the command to invoke the application
+THEN my server is started and the Sequelize models are synced to the MySQL database
+WHEN I open API GET routes in Insomnia Core for categories, products, or tags
+THEN the data for each of these routes is displayed in a formatted JSON
+WHEN I test API POST, PUT, and DELETE routes in Insomnia Core
+THEN I am able to successfully create, update, and delete data in my database
+```
+
+## Developer
+**Taylor Bowen** 
+* [GitHub](https://github.com/tbowenmsu)
+* [LinkedIn](https://www.linkedin.com/in/taylor-bowen6/)
+* [Portfolio](https://tbowenmsu.github.io/react-portfolio/)
+
+## Built with
+* MySQL Database
+* MySQL2
+* Sequelize
+* JavaScript
+* Postman API Tester
+* NPM
+* Node
+* Express
+
+
+## Demo Videos
+**Application Setup:**
+* [Creating and Seeding Database](https://youtu.be/MUyA9lkV3mg)
+
+**Postman Demos:**
+* [GET Routes](https://youtu.be/eUDkD8ZjHMc)
+* [Product Routes](https://youtu.be/b1Xg82opi6c)
+* [Tag Routes](https://youtu.be/HMl7VGxWwOc)
+* [Category Routes](https://youtu.be/o6sWwsrO6yg)
+
+
+
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,29 +6,51 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      Category,
+      {
+        model: Tag,
+        through: ProductTag,
+      },
+    ],
+  })
+    .then((products) => res.json(products))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      Category,
+      {
+        model: Tag,
+        through: ProductTag,
+      },
+    ],
+  })
+    .then((products) => res.json(products))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds && req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -91,6 +113,19 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((products) => {
+      console.log(products);
+      res.json(products);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 module.exports = router;
